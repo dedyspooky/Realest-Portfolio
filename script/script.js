cd886b4667074f0a34a9c1fa69aa929fe2551229
Aviral Ale <aviralale@gmail.com>
2023-05-25 10:57:24 +0545

    bugs

--- script/script.js
+++ script/script.js
@@ -190,35 +190,3 @@ btn.addEventListener('click', () => {
 window.addEventListener('load', () => {
   chevron.style.transform = 'rotate(0deg)';
 })
-
-
-
-// T and C
-const leftButton = document.getElementById('leftbtn');
-const rightButton = document.getElementById('rightbtn');
-const termsConditions = document.getElementById('tandc');
-const privacyPolicy = document.getElementById('privacy-policy');
-
-window.addEventListener('load', ()=>{
-    privacyPolicy.style.transform = 'translateX(1000em)';
-    leftButton.style.opacity = '0';
-        leftButton.style.scale = '0';
-})
-
-rightButton.addEventListener('click',()=>{
-    termsConditions.style.transform = 'translateX(-1000em)';
-    privacyPolicy.style.transform = 'translateX(0em)';
-    leftButton.style.opacity = '1';
-    leftButton.style.scale = '1';
-    rightButton.style.opacity = '0';
-    rightButton.style.scale = '0';
-    
-    leftButton.addEventListener('click',()=>{
-        privacyPolicy.style.transform = 'translateX(1000em)';
-        termsConditions.style.transform = 'translateX(0em)';
-        leftButton.style.opacity = '0';
-        leftButton.style.scale = '0';
-        rightButton.style.opacity = '1';
-        rightButton.style.scale = '1';
-    })
-})
